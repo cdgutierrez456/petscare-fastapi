114ae2ecb1c6b123c92e6278807488b7b0ad66a4
Cristian Gutierrez <cdgutierrez456@gmail.com>
2025-11-05 21:16:40 -0500

    Agregando creacion de tablas

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.pet import router as pet_router
 from app.routes.auth import router as auth_router
 from app.routes.user import router as user_router
+from .database import create_tables
+import app.models
 
 app = FastAPI()
 
@@ -22,6 +24,10 @@ app.add_middleware(
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(pet_router, prefix="/api/pets", tags=["Pets"])
 app.include_router(user_router, prefix="/api/user", tags=["Users"])
+
+@app.on_event("startup")
+async def on_startup():
+    await create_tables()
 
 @app.get("/")
 def read_root():
